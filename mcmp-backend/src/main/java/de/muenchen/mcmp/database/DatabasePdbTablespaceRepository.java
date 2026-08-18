22af64a5c09c47b1c2142a2c1877b7da00ba9903
Alexander Heidrich <116089635+euerla@users.noreply.github.com>
2026-08-18 15:15:35 +0200

    refactor(repositories): remove unused `List` imports from repository interfaces

--- mcmp-backend/src/main/java/de/muenchen/mcmp/database/DatabasePdbTablespaceRepository.java
+++ mcmp-backend/src/main/java/de/muenchen/mcmp/database/DatabasePdbTablespaceRepository.java
@@ -2,8 +2,6 @@ package de.muenchen.mcmp.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DatabasePdbTablespaceRepository extends JpaRepository<DatabasePdbTablespace, Long> {
 
 }
--- mcmp-backend/src/main/java/de/muenchen/mcmp/database/DatabasePdbUserRepository.java
+++ mcmp-backend/src/main/java/de/muenchen/mcmp/database/DatabasePdbUserRepository.java
@@ -2,8 +2,6 @@ package de.muenchen.mcmp.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DatabasePdbUserRepository extends JpaRepository<DatabasePdbUser, Long> {
 
 }
